9e55e7aa940523af9d366773c6c4996da1233d68
mattyc246 <matty_c_246@gmail.com>
2018-11-15 15:32:35 +0800

    added no of guests to reservations table

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,2 +1,14 @@
 class ReservationsController < ApplicationController
+
+  def create
+
+  end
+
+  private
+
+  def reservation_params
+
+    params.require(:reservation).permit(:check_in, :check_out)
+
+  end
 end
