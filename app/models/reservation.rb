b682284dac7a85f912bec34f583746acd25f5bfa
mattyc246 <matty_c_246@gmail.com>
2018-11-17 12:25:45 +0800

    reservations validations done

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,30 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :listing
+
+  validates :check_in, presence: true, on: [:create]
+  validates :check_out, presence: true, on: [:create]
+  validates :guests, presence: true, on: [:create]
+  validate :no_overlapping_dates, on: [:create]
+
+  def no_overlapping_dates
+
+    listing = Listing.find(self.listing_id)
+
+    listing.reservations.each do |res|
+
+      if (self.check_in..self.check_out).overlaps?(res.check_in..res.check_out) && res != self
+
+        return true
+
+      else
+
+        return false
+
+      end
+
+    end
+
+  end
+
 end
