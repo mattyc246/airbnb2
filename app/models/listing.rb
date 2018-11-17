fbbde34cb5142b7e813f21445910a0b910afb2fe
mattyc246 <matty_c_246@gmail.com>
2018-11-17 12:17:23 +0800

    listing validations complete

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,6 +4,18 @@ class Listing < ApplicationRecord
   has_many :tags
   has_many :reviews
 
+  validates :title, presence: true, on: [:create]
+  validates :property_type, presence: true, on: [:create]
+  validates :guests, presence: true, on: [:create]
+  validates :bedrooms, presence: true, on: [:create]
+  validates :bathrooms, presence: true, on: [:create]
+  validates :price_per_night, presence: true, on: [:create]
+  validates :address_line1, presence: true, on: [:create]
+  validates :city, presence: true, on: [:create]
+  validates :state, presence: true, on: [:create]
+  validates :postcode, presence: true, on: [:create]
+  validates :country, presence: true, on: [:create]
+
   def self.property_type
 
     ["House", "Shared Accomodation", "Hostel", "Hotel"]
